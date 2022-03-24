086594d00f40d8ba56cabeb6e4ae304e6b94ec9e
Vanshul Kesharwani <vkvanshulkesharwani54@gmail.com>
2022-03-24 12:42:51 +0530

    Version 1.0.0 : Initial Commit.

---
+++ JS/script.js
@@ -0,0 +1,8 @@
+/*
+Website Name - News Website
+Name : Vanshul Kesharwani
+Date : 24/03/2022
+Version : 1.0.0
+Email : vkvanshulkesharwani54@gmail.com
+Description : This is a Web app for latest news. Fetching from news API website.
+*/
